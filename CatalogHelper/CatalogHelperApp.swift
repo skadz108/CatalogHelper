52ae388aaae3ee06358a8fb2cbc0bc2dd23ae8d2
Skadz <skadz@skadz.online>
2024-12-04 14:58:55 -0700

    Initial Commit

---
+++ CatalogHelper/CatalogHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogHelperApp.swift
+//  CatalogHelper
+//
+//  Created by Skadz on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CatalogHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
